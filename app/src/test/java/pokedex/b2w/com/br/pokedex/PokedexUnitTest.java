465b2dd405926c13dc56ba2f8094305fd9a18695
celso de oliveira <celso.a.oliveira@gmail.com>
2020-05-16 18:58:55 -0300

    Testes

--- app/src/test/java/pokedex/b2w/com/br/pokedex/PokedexUnitTest.java
+++ app/src/test/java/pokedex/b2w/com/br/pokedex/PokedexUnitTest.java
@@ -12,9 +12,7 @@ import pokedex.b2w.com.br.pokedex.objects.service.PokedexService;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @author celso
  */
 public class PokedexUnitTest {
 
